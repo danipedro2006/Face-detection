e53514bff055217c8755202fe97ed3caa1b8ea57
danipedro2006 <danipedro2006@yahoo.com>
2019-12-19 14:34:43 +0200

    first commit

---
+++ src/com/dani/facedetection/app/App.java
@@ -0,0 +1,26 @@
+package com.dani.facedetection.app;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.dani.facedetection.gui.MainFrame;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new MainFrame();
+			}
+		});
+	}
+}
